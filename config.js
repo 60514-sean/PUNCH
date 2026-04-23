947c92e7a75f7818adb4f8f1a54ad18159b3aff2
KK <sean605147@email.com>
2026-04-23 10:26:46 +0800

    config: 填入 GAS 網址

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 填入 Google Apps Script 部署網址
 // 格式：https://script.google.com/macros/s/XXXXXXXXXX/exec
-window.GAS_URL = '';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbx5uFoCCU9X5czLKzXHOm3WRXuyCozGg2xfIuuLQ06xSxcz-Bq-QgNpIpjXrD9FbFjOlQ/exec';
